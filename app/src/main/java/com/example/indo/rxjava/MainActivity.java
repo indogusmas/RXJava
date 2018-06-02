569a23941d1b2fb5d3cdf3640a0f26f6188bcdf9
indogusmas <indogusmasarungsamudra@gmail.com>
2018-06-02 22:17:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/indo/rxjava/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.indo.rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.reactivex.Flowable;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    Button btnRx;
+    TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnRx =(Button) findViewById(R.id.buttonrx);
+        result = (TextView)findViewById(R.id.tv_textview);
+        btnRx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    doSomeWork();
+            }
+        });
+    }
+    /*
+    *Example using Flowable RXjava
+     */
+    private void doSomeWork(){
+        Flowable<Integer> observable = Flowable.just(1,2);
+
+        observable.reduce(9, new BiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer t1, Integer t2) throws Exception {
+                return t1 + t2;
+            }
+        }).subscribe(getObserver());
+    }
+
+    private SingleObserver<Integer> getObserver(){
+
+        return  new SingleObserver<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, "onSubscribe: "+ d.isDisposed() );
+            }
+
+            @Override
+            public void onSuccess(Integer integer) {
+                Log.e(TAG, "onSuccess: " + integer.toString() );
+                result.setText(integer.toString());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage() );
+                result.setText(e.getMessage());
+            }
+        };
+    }
+}
